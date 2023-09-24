184fd5776a71767eb30612b8fea7b7f8b6d0c7c5
Kostas Vlachos <kvl_93@outlook.com>
2023-09-24 17:06:09 +0300

    refactor(bld_core/src/scanner/mod.rs): Changed scanner to spawn a task for a receiver in order to have a queue of reads and change the use of std::fs to tokio::fs

--- crates/bld_core/src/scanner/mod.rs
+++ crates/bld_core/src/scanner/mod.rs
@@ -1,43 +1,102 @@
+use actix::spawn;
+use anyhow::{anyhow, Result};
 use bld_config::BldConfig;
-use std::fs::File;
-use std::io::{BufRead, BufReader};
-use std::path::PathBuf;
-use std::sync::Arc;
+use std::{path::PathBuf, sync::Arc};
+use tokio::{
+    fs::File,
+    io::{AsyncBufReadExt, BufReader},
+    sync::{
+        mpsc::{channel, Receiver, Sender},
+        oneshot,
+    },
+};
+use tracing::error;
 
-pub struct FileScanner {
-    path: PathBuf,
-    file_handle: Option<File>,
+enum FileScannerMessage {
+    Next(oneshot::Sender<Vec<String>>),
 }
 
-impl FileScanner {
-    pub fn new(cfg: Arc<BldConfig>, run_id: &str) -> Self {
+struct FileScannerReceiver {
+    path: PathBuf,
+    file_handle: Option<File>,
+    receiver: Receiver<FileScannerMessage>,
+}
+
+impl FileScannerReceiver {
+    pub fn new(
+        config: Arc<BldConfig>,
+        run_id: &str,
+        receiver: Receiver<FileScannerMessage>,
+    ) -> Self {
         Self {
-            path: cfg.log_full_path(run_id),
+            path: config.log_full_path(run_id),
             file_handle: None,
+            receiver,
         }
     }
 
-    fn try_open(&mut self) {
+    pub async fn receive(&mut self) -> Result<()> {
+        while let Some(msg) = self.receiver.recv().await {
+            match msg {
+                FileScannerMessage::Next(resp_tx) => self.next(resp_tx).await?,
+            }
+        }
+        Ok(())
+    }
+
+    async fn try_open(&mut self) {
         if self.file_handle.is_some() {
             return;
         }
         self.file_handle = match self.path.is_file() {
-            true => File::open(&self.path).map(Some).unwrap_or(None),
+            true => File::open(&self.path).await.map(Some).unwrap_or(None),
             false => None,
         };
     }
 
-    pub fn scan(&mut self) -> Vec<String> {
-        self.try_open();
+    async fn next(&mut self, resp_tx: oneshot::Sender<Vec<String>>) -> Result<()> {
+        self.try_open().await;
+
         let mut content: Vec<String> = vec![];
-        if let Some(file_handle) = &self.file_handle {
-            let reader = BufReader::new(file_handle);
-            for (_i, line) in reader.lines().enumerate() {
-                if let Ok(line) = line {
-                    content.push(line);
-                }
-            }
+        let Some(file_handle) = self.file_handle.as_mut() else {
+            resp_tx.send(content).map_err(|_| anyhow!("oneshot response sender dropped"))?;
+            return Ok(());
+        };
+
+        let reader = BufReader::new(file_handle);
+        let mut lines = reader.lines();
+        let mut next = lines.next_line().await?;
+        while let Some(line) = next {
+            content.push(line);
+            next = lines.next_line().await?;
         }
-        content
+
+        resp_tx.send(content).map_err(|_| anyhow!("oneshot response sender dropped"))?;
+        Ok(())
+    }
+}
+
+pub struct FileScanner {
+    tx: Sender<FileScannerMessage>,
+}
+
+impl FileScanner {
+    pub fn new(config: Arc<BldConfig>, run_id: &str) -> Self {
+        let (tx, rx) = channel(4096);
+        let mut receiver = FileScannerReceiver::new(config, run_id, rx);
+
+        spawn(async move {
+            if let Err(e) = receiver.receive().await {
+                error!("{e}");
+            }
+        });
+
+        Self { tx }
+    }
+
+    pub async fn scan(&mut self) -> Result<Vec<String>> {
+        let (resp_tx, resp_rx) = oneshot::channel();
+        self.tx.send(FileScannerMessage::Next(resp_tx)).await?;
+        resp_rx.await.map_err(|e| anyhow!(e))
     }
 }
