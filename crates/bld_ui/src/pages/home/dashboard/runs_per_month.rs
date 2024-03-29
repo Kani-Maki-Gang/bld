a18b819b4815a446fa6b54af757db6f10f9c6a48
Kostas Vlachos <kvl_93@outlook.com>
2024-03-29 23:49:22 +0200

    feat(runs_per_month.rs): Added leptos_chartistry and added a demo bar chart

--- crates/bld_ui/src/pages/home/dashboard/runs_per_month.rs
+++ crates/bld_ui/src/pages/home/dashboard/runs_per_month.rs
@@ -1,28 +1,51 @@
 use crate::components::card::Card;
 use leptos::*;
-use chart_js_rs::{bar::Bar, Dataset, NoAnnotations, XYDataset};
+use leptos_chartistry::*;
+
+struct MyData {
+    pub month: f64,
+    pub runs: f64,
+}
 
 #[component]
 pub fn DashboardRunsPerMonth() -> impl IntoView {
-    let months = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-    let chart = Bar::<NoAnnotations> {
-        data: Dataset {
-            labels: Some(months.iter().map(|x| x.into()).collect()),
-            datasets: vec![
-                XYDataset{
-                    label: Some("Runs".into()),
-                    data: vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12],
-                    ..Default::default()
-                }
-            ]
-        }
-        ..Default::default()
-    };
+    let series = Series::new(|data: &MyData| data.month)
+        .with_min_y(0.0)
+        .with_max_y(150.0)
+        .bar(|data: &MyData| data.runs);
+
+    let (data, _set_data) = create_signal(vec![
+        MyData { month: 1.0, runs: 10.0 },
+        MyData { month: 2.0, runs: 20.0 },
+        MyData { month: 3.0, runs: 30.0 },
+        MyData { month: 4.0, runs: 40.0 },
+        MyData { month: 5.0, runs: 50.0 },
+        MyData { month: 6.0, runs: 60.0 },
+        MyData { month: 7.0, runs: 70.0 },
+        MyData { month: 8.0, runs: 80.0 },
+        MyData { month: 9.0, runs: 90.0 },
+        MyData { month: 10.0, runs: 100.0 },
+        MyData { month: 11.0, runs: 110.0 },
+        MyData { month: 12.0, runs: 120.0 },
+    ]);
+
     view! {
         <Card>
             <div class="flex flex-col px-8 py-12">
-                <div class="text-2xl">"Runs per month"</div>
-                <div class="text-gray-400 mb-8">"Overview"</div>
+                <div class="text-2xl">"Total runs per month"</div>
+                <div class="text-gray-400 mb-8">"Aggregated data for all pipelines on the server"</div>
+                <div class="grow">
+                    <Chart
+                        aspect_ratio=AspectRatio::from_inner_ratio(1150.0, 500.0)
+                        series=series
+                        data=data
+                        left=TickLabels::aligned_floats()
+                        inner=[
+                            AxisMarker::left_edge().into_inner(),
+                            AxisMarker::bottom_edge().into_inner(),
+                        ]
+                    />
+                </div>
             </div>
         </Card>
     }
