c02644dcc77d124427b948e08b75e177949f3abf
Kostas Vlachos <kvl_93@outlook.com>
2024-04-10 17:24:48 +0300

    feat(pipelines/info.rs): Implemented the request for the pipeline info with the query params from the url param

--- crates/bld_ui/src/pages/home/pipelines/info.rs
+++ crates/bld_ui/src/pages/home/pipelines/info.rs
@@ -1,5 +1,30 @@
+use anyhow::Result;
+use bld_models::dtos::PipelineQueryParams;
 use leptos::*;
 use leptos_router::*;
+use reqwest::Client;
+use serde_json::Value;
+
+async fn get_pipeline(id: String) -> Result<Value> {
+    let params = PipelineQueryParams {
+        pipeline: id
+    };
+
+    let res = Client::builder()
+        .build()?
+        .get("http://localhost:6080/v1/print")
+        .header("Accept", "application/json")
+        .query(&params)
+        .send()
+        .await?;
+
+    if res.status().is_success() {
+        let body = res.text().await?;
+        Ok(serde_json::from_str(&body)?)
+    } else {
+        Ok(Value::Null)
+    }
+}
 
 #[component]
 pub fn PipelineInfo() -> impl IntoView {
@@ -8,6 +33,25 @@ pub fn PipelineInfo() -> impl IntoView {
     let pipeline_id = move || {
         params.with(|p| p.get("id").cloned())
     };
+
+    let (_data, set_data) = create_signal(Value::Null);
+
+    let _ = create_resource(
+        move || (pipeline_id(), set_data),
+        |(id, set_data)| async move {
+            let Some(id) = id else {
+                return;
+            };
+
+            let value = get_pipeline(id)
+                .await
+                .unwrap_or_else(|_| Value::Null);
+
+            if value == Value::Null {
+                set_data.set(value);
+            }
+        }
+    );
 
     view! {
         "Hello from the pipeline info page for pipeline with id: " {pipeline_id}
