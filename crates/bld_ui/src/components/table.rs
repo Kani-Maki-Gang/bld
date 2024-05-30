bedda64a09ec303523a7e3b7913b732b6aeb34ca
Kostas Vlachos <kvl_93@outlook.com>
2024-05-31 02:42:05 +0300

    feat(components/table.rs): Added a new component that uses children to display a table

--- crates/bld_ui/src/components/table.rs
+++ crates/bld_ui/src/components/table.rs
@@ -48,3 +48,61 @@ pub fn Table(
         </div>
     }
 }
+
+#[component]
+pub fn DataTable(children: Children) -> impl IntoView {
+    view! {
+        <div class="overflow-x-auto">
+            <table class="min-w-full bg-slate-700 text-sm">
+                {children()}
+            </table>
+        </div>
+    }
+}
+
+#[component]
+pub fn Headers(children: Children) -> impl IntoView {
+    view! {
+        <thead>
+            <tr>
+                {children()}
+            </tr>
+        </thead>
+    }
+}
+
+#[component]
+pub fn Header(children: Children) -> impl IntoView {
+    view! {
+        <th class="border border-b-4 border-slate-600 whitespace-nowrap p-4 font-bold text-left">
+            {children()}
+        </th>
+    }
+}
+
+#[component]
+pub fn Body(children: Children) -> impl IntoView {
+    view! {
+        <tbody>
+            {children()}
+        </tbody>
+    }
+}
+
+#[component]
+pub fn Row(children: Children) -> impl IntoView {
+    view! {
+        <tr>
+            {children()}
+        </tr>
+    }
+}
+
+#[component]
+pub fn Cell(children: Children) -> impl IntoView {
+    view! {
+        <td class="border border-slate-600 whitespace-nowrap p-4 text-left">
+            {children()}
+        </td>
+    }
+}
