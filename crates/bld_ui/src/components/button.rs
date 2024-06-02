262856a30dbd72e040ff64e68a3db225f37d3a10
Kostas Vlachos <kvl_93@outlook.com>
2024-06-02 21:00:21 +0300

    feat(components/buttons.rs): Added new IconButton component

--- crates/bld_ui/src/components/button.rs
+++ crates/bld_ui/src/components/button.rs
@@ -1,10 +1,51 @@
 use leptos::*;
 
+pub enum ButtonMode {
+    Default,
+    Success,
+    Danger
+}
+
+impl Default for ButtonMode {
+    fn default() -> Self {
+        ButtonMode::Default
+    }
+}
+
+fn get_mode_classes(mode: ButtonMode) -> &'static str {
+    match mode {
+        ButtonMode::Default => "bg-indigo-600 hover:bg-indigo-700 focus:bg-indigo-700",
+        ButtonMode::Success => "bg-green-600 hover:bg-green-700 focus:bg-green-700",
+        ButtonMode::Danger => "bg-red-600 hover:bg-red-700 focus:bg-red-700",
+    }
+}
+
 #[component]
-pub fn Button(children: Children) -> impl IntoView {
+pub fn Button(
+    #[prop(optional)] mode: ButtonMode,
+    #[prop(into, optional)] class: String,
+    children: Children
+) -> impl IntoView {
+    let mode = get_mode_classes(mode);
+    let class=format!("h-[40px] w-full flex-none rounded-lg p-2 focus:outline-none {mode} {class}");
     view! {
-        <button class="h-[40px] flex-non text-white rounded-lg w-full bg-indigo-600 p-2 hover:bg-indigo-700 focus:bg-indigo-700 focus:outline-none">
+        <button class=class>
             {children()}
+        </button>
+    }
+}
+
+#[component]
+pub fn IconButton(
+    #[prop(optional)] mode: ButtonMode,
+    #[prop(into, optional)] class: String,
+    #[prop(into)] icon: String
+) -> impl IntoView {
+    let mode = get_mode_classes(mode);
+    let class=format!("h-[40px] w-[40px] flex-none text-xl rounded-lg p-2 focus:outline-none {mode} {class}");
+    view! {
+        <button class=class>
+            <i class=icon />
         </button>
     }
 }
