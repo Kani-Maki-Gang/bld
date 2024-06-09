3f270614c74bf7fda8be0be249d34ff5d566e0b7
Kostas Vlachos <kvl_93@outlook.com>
2024-06-09 19:58:42 +0300

    refactor(pipelines/mod.rs): Changed the location of flex elements to start

--- crates/bld_ui/src/pages/home/pipelines/mod.rs
+++ crates/bld_ui/src/pages/home/pipelines/mod.rs
@@ -25,7 +25,7 @@ pub fn Pipelines() -> impl IntoView {
             <div class="container">
                 <Card>
                     <div class="flex flex-col px-8 py-12">
-                        <div class="flex justify-items-center gap-x-4 pr-2 items-center">
+                        <div class="flex items-start gap-x-4 pr-2">
                             <div class="grow flex flex-col">
                                 <div class="text-2xl">
                                     "Pipelines"
