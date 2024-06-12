f9be27cad03257c999d9a701e7efd68c0a3b025e
Kostas Vlachos <kvl_93@outlook.com>
2024-06-12 21:01:29 +0300

    refactor(error.rs): Changed error text

--- crates/bld_ui/src/error.rs
+++ crates/bld_ui/src/error.rs
@@ -6,7 +6,7 @@ pub fn Error(#[prop(into)] error: Signal<String>) -> impl IntoView {
         <div class="text-red-500 text-center text-8xl">
             <i class="iconoir-cloud-xmark"></i>
         </div>
-        <div class="text-center">"Failed to fetch pipelines due to: " {move || error.get()}</div>
+        <div class="text-center">"Failed to fetch data due to: " {move || error.get()}</div>
         <div class="text-center text-gray-400">"Please try again later"</div>
     }
 }
