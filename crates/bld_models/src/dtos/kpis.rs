73f5cddbed4f2d6804a8a09b89a73d578255eef7
Kostas Vlachos <kvl_93@outlook.com>
2024-08-03 22:01:03 +0300

    refactor(dtos/kpis.rs): Seperated the response for each kpi to each own struct

--- crates/bld_models/src/dtos/kpis.rs
+++ crates/bld_models/src/dtos/kpis.rs
@@ -1,13 +1,18 @@
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct KpiInfo {
+pub struct QueuedPipelinesKpi {
     pub count: u64,
-    pub percentage: f64,
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize)]
-pub struct CompletedPipelinesKpiInfo {
+pub struct RunningPipelinesKpi {
+    pub count: i64,
+    pub available_workers: i64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct CompletedPipelinesKpi {
     pub finished_count: i64,
     pub faulted_count: i64,
     pub finished_percentage: f64,
