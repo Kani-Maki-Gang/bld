d4e5de3d38f713962913124a3c7bce96f81f3360
Kostas Vlachos <kvl_93@outlook.com>
2024-08-03 21:40:40 +0300

    feat(endpoints/ui.rs): Added new endpoint for fetching kpis for completed pipelines

--- crates/bld_server/src/endpoints/ui.rs
+++ crates/bld_server/src/endpoints/ui.rs
@@ -1,7 +1,10 @@
 use actix_web::{get, web::Data, HttpResponse, Responder};
 use anyhow::Result;
 use bld_config::BldConfig;
-use bld_models::{dtos::KpiInfo, pipeline_runs};
+use bld_models::{
+    dtos::{CompletedPipelinesKpiInfo, KpiInfo},
+    pipeline_runs,
+};
 use chrono::{Duration, Utc};
 use sea_orm::DatabaseConnection;
 use tracing::info;
@@ -15,12 +18,12 @@ async fn get_count_of_queued_pipelines(conn: &DatabaseConnection) -> Result<KpiI
 
     let count = current_days_count - previous_days_count;
 
-    let percentage = (current_days_count as i32)
-        .checked_div(previous_days_count as i32)
+    let percentage = (current_days_count as i64)
+        .checked_div(previous_days_count as i64)
         .map(|x| x as f64 * 100.0)
         .unwrap_or(0.0);
 
-    Ok(KpiInfo::new(count, percentage))
+    Ok(KpiInfo { count, percentage })
 }
 
 #[get("/v1/ui/kpis/queued-pipelines")]
@@ -41,7 +44,7 @@ async fn get_count_of_running_pipelines(
 ) -> Result<KpiInfo> {
     pipeline_runs::count_running(&conn).await.map(|count| {
         let percentage = config.local.supervisor.workers as f64 - count as f64;
-        KpiInfo::new(count, percentage)
+        KpiInfo { count, percentage }
     })
 }
 
@@ -55,6 +58,36 @@ pub async fn running_pipelines(
         Ok(kpi) => HttpResponse::Ok().json(kpi),
         Err(e) => {
             info!("could not get the count of running pipelines due to: {e}");
+            HttpResponse::BadRequest().body("")
+        }
+    }
+}
+
+async fn get_completed_pipelines(conn: &DatabaseConnection) -> Result<CompletedPipelinesKpiInfo> {
+    let count_per_state = pipeline_runs::count_per_state_last_ten_days(conn).await?;
+
+    let finished_percentage = (count_per_state.finished as i64)
+        .checked_div((count_per_state.finished + count_per_state.faulted) as i64)
+        .map(|x| x as f64 * 100.0)
+        .unwrap_or(0.0);
+
+    let faulted_percentage = 100.0 - finished_percentage;
+
+    Ok(CompletedPipelinesKpiInfo {
+        finished_count: count_per_state.finished,
+        faulted_count: count_per_state.faulted,
+        finished_percentage,
+        faulted_percentage,
+    })
+}
+
+#[get("/v1/ui/kpis/completed-pipelines")]
+pub async fn completed_pipelines(conn: Data<DatabaseConnection>) -> impl Responder {
+    info!("Reached handler for /v1/ui/kpis/completed-pipelines route");
+    match get_completed_pipelines(&conn).await {
+        Ok(kpi) => HttpResponse::Ok().json(kpi),
+        Err(e) => {
+            info!("could not get the count of completed pipelines due to: {e}");
             HttpResponse::BadRequest().body("")
         }
     }
