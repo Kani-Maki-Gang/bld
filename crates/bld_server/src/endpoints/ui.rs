16f6870487d4bcc8de1dadb8bde5ec76a2adc687
Kostas Vlachos <kvl_93@outlook.com>
2024-08-03 17:43:34 +0300

    feat(endpoints/ui.rs): Added new endpoint for fetching the kpi info for running pipelines

--- crates/bld_server/src/endpoints/ui.rs
+++ crates/bld_server/src/endpoints/ui.rs
@@ -1,5 +1,6 @@
 use actix_web::{get, web::Data, HttpResponse, Responder};
 use anyhow::Result;
+use bld_config::BldConfig;
 use bld_models::{dtos::KpiInfo, pipeline_runs};
 use chrono::{Duration, Utc};
 use sea_orm::DatabaseConnection;
@@ -7,12 +8,10 @@ use tracing::info;
 
 async fn get_count_of_queued_pipelines(conn: &DatabaseConnection) -> Result<KpiInfo> {
     let previous_date_time = (Utc::now() - Duration::days(10)).naive_utc();
-    let previous_days_count =
-        pipeline_runs::count_pipelines_runs_on_date(conn, previous_date_time).await?;
+    let previous_days_count = pipeline_runs::count_queued_on_date(conn, previous_date_time).await?;
 
     let current_date_time = Utc::now().naive_utc();
-    let current_days_count =
-        pipeline_runs::count_pipelines_runs_on_date(conn, current_date_time).await?;
+    let current_days_count = pipeline_runs::count_queued_on_date(conn, current_date_time).await?;
 
     let count = current_days_count - previous_days_count;
 
@@ -31,6 +30,31 @@ pub async fn queued_pipelines(conn: Data<DatabaseConnection>) -> impl Responder 
         Ok(kpi) => HttpResponse::Ok().json(kpi),
         Err(e) => {
             info!("could not get the count of queued pipelines due to: {e}");
+            HttpResponse::BadRequest().body("")
+        }
+    }
+}
+
+async fn get_count_of_running_pipelines(
+    config: &BldConfig,
+    conn: &DatabaseConnection,
+) -> Result<KpiInfo> {
+    pipeline_runs::count_running(&conn).await.map(|count| {
+        let percentage = config.local.supervisor.workers as f64 - count as f64;
+        KpiInfo::new(count, percentage)
+    })
+}
+
+#[get("/v1/ui/kpis/running-pipelines")]
+pub async fn running_pipelines(
+    config: Data<BldConfig>,
+    conn: Data<DatabaseConnection>,
+) -> impl Responder {
+    info!("Reached handler for /v1/ui/kpis/running-pipelines route");
+    match get_count_of_running_pipelines(&config, &conn).await {
+        Ok(kpi) => HttpResponse::Ok().json(kpi),
+        Err(e) => {
+            info!("could not get the count of running pipelines due to: {e}");
             HttpResponse::BadRequest().body("")
         }
     }
