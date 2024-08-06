25f26c009988a82d3342ac283c455033d7863471
Kostas Vlachos <kvl_93@outlook.com>
2024-08-06 20:20:57 +0300

    feat(dashboard/runs_per_month.rs): Implemented the component with data from the run per month endpoint

--- crates/bld_ui/src/pages/home/dashboard/runs_per_month.rs
+++ crates/bld_ui/src/pages/home/dashboard/runs_per_month.rs
@@ -1,89 +1,62 @@
-use crate::components::card::Card;
+use crate::{api, components::card::Card, error::SmallError};
+use bld_models::dtos::PipelineRunsPerMonthKpi;
 use leptos::*;
 use leptos_chartistry::*;
 
-struct MyData {
-    pub month: f64,
-    pub runs: f64,
-}
-
 #[component]
 pub fn DashboardRunsPerMonth() -> impl IntoView {
-    let series = Series::new(|data: &MyData| data.month)
+    let series = Series::new(|data: &PipelineRunsPerMonthKpi| data.month)
         .with_min_y(0.0)
         .with_max_y(150.0)
-        .bar(|data: &MyData| data.runs);
+        .bar(|data: &PipelineRunsPerMonthKpi| data.count);
 
-    let (data, _set_data) = create_signal(vec![
-        MyData {
-            month: 1.0,
-            runs: 10.0,
+    let data = create_resource(
+        || (),
+        |_| async move {
+            api::pipeline_runs_per_month()
+                .await
+                .map_err(|e| e.to_string())
         },
-        MyData {
-            month: 2.0,
-            runs: 20.0,
-        },
-        MyData {
-            month: 3.0,
-            runs: 30.0,
-        },
-        MyData {
-            month: 4.0,
-            runs: 40.0,
-        },
-        MyData {
-            month: 5.0,
-            runs: 50.0,
-        },
-        MyData {
-            month: 6.0,
-            runs: 60.0,
-        },
-        MyData {
-            month: 7.0,
-            runs: 70.0,
-        },
-        MyData {
-            month: 8.0,
-            runs: 80.0,
-        },
-        MyData {
-            month: 9.0,
-            runs: 90.0,
-        },
-        MyData {
-            month: 10.0,
-            runs: 100.0,
-        },
-        MyData {
-            month: 11.0,
-            runs: 110.0,
-        },
-        MyData {
-            month: 12.0,
-            runs: 120.0,
-        },
-    ]);
+    );
+
+    let chart_data = move || {
+        if let Some(Ok(data)) = data.get() {
+            series.max_y.set(
+                data.iter()
+                    .map(|x| x.count as i64)
+                    .max()
+                    .map(|x| x as f64),
+            );
+            data
+        } else {
+            vec![]
+        }
+    };
 
     view! {
         <Card>
-            <div class="flex flex-col px-8 py-12">
-                <div class="text-2xl">"Total runs per month"</div>
-                <div class="text-gray-400 mb-8">
-                    "Aggregated data for all pipelines on the server"
+            <div class="flex flex-col px-8 py-12 gap-4">
+                <div class="flex flex-col">
+                    <div class="text-2xl">"Total runs per month"</div>
+                    <div class="text-gray-400 mb-8">
+                        "Aggregated data for all pipelines on the server"
+                    </div>
                 </div>
+                <Show when=move || matches!(data.get(), Some(Err(_))) fallback=|| view! {}>
+                    <SmallError error=move || data.get().unwrap().unwrap_err()/>
+                </Show>
                 <div class="grow">
                     <Chart
                         aspect_ratio=AspectRatio::from_inner_ratio(1150.0, 500.0)
                         series=series
-                        data=data
+                        data=move || chart_data()
                         left=TickLabels::aligned_floats()
+                        bottom=TickLabels::aligned_floats()
                         inner=[
                             AxisMarker::left_edge().into_inner(),
                             AxisMarker::bottom_edge().into_inner(),
                         ]
                     />
-
                 </div>
             </div>
         </Card>
