d8482166175883ef8185ff5de0393f73687a195d
Kostas Vlachos <kvl_93@outlook.com>
2024-11-10 17:01:25 +0200

    refactor(pipeline/traits.rs): Added new Dependencies trait

--- crates/bld_runner/src/pipeline/traits.rs
+++ crates/bld_runner/src/pipeline/traits.rs
@@ -1,6 +1,11 @@
 use anyhow::Result;
+use bld_config::BldConfig;
 
 pub trait Load<T> {
     fn load(input: &str) -> Result<T>;
     fn load_with_verbose_errors(input: &str) -> Result<T>;
+}
+
+pub trait Dependencies {
+    fn local_deps(&self, config: &BldConfig) -> Vec<String>;
 }
