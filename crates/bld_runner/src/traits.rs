dada4ab615357a7280e974bf793c8dcacc526bd5
Kostas Vlachos <kvl_93@outlook.com>
2024-11-11 20:29:33 +0200

    feat(traits.rs): Added

---
+++ crates/bld_runner/src/traits.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+use anyhow::Result;
+use bld_config::BldConfig;
+
+pub trait Load<T> {
+    fn load(input: &str) -> Result<T>;
+    fn load_with_verbose_errors(input: &str) -> Result<T>;
+}
+
+pub trait Dependencies {
+    fn local_deps(&self, config: &BldConfig) -> Vec<String>;
+}
+
+#[allow(async_fn_in_trait)]
+pub trait Validate {
+    async fn validate(self) -> Result<()>;
+}
+
+pub type Variables = (HashMap<String, String>, HashMap<String, String>);
+
+pub trait IntoVariables {
+    fn into_variables(self) -> Variables;
+}
